8393c2190afdc264778e8fdd54d6fa2373311664
Sara (Risclover) <sara091592@gmail.com>
2024-08-24 22:43:14 -0700

    Remove unwanted files

--- src/components/CircleBtn/CircleBtn.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import MenuIcon from "../../assets/images/icon-menu.svg"; // Import the SVG correctly
-import "./CircleBtn.css";
-export const CircleBtn = ({ value, onClick }) => {
-    return (_jsx("button", { className: `circle-btn${value === MenuIcon ? " centered" : ""}`, onClick: onClick, children: _jsx("img", { src: value, alt: "icon" }) }));
-};
