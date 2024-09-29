55fe1c88e4308430fa4d727d562c5ab2dc4c77f0
Risclover <sara091592@gmail.com>
2024-09-29 04:10:32 -0700

    Remove unnecessary files

--- src/components/HowToPlayStep/HowToPlayStep.d.ts
+++
@@ -1,9 +0,0 @@
-import "./HowToPlayStep.css";
-type Props = {
-    id: string;
-    number: string;
-    title: string;
-    content: string;
-};
-export declare const HowToPlayStep: ({ id, number, title, content }: Props) => import("react/jsx-runtime").JSX.Element;
-export {};
